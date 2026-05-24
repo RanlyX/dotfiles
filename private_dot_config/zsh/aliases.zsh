307cb8ab1606d0a1f7badabef54e27b416049451
Ranly Haung <et82122@gmail.com>
2026-05-25 01:37:03 +0800

    Add zsh config

---
+++ private_dot_config/zsh/aliases.zsh
@@ -0,0 +1,57 @@
+# ========================================================
+# zsh
+# ========================================================
+alias rlz="source $ZDOTDIR/.zshrc"
+alias edz="nvim $ZDOTDIR"
+
+# Better ls
+alias ls='eza'
+
+# Detailed listing
+alias ll='eza -lh --git'
+
+# Detailed listing including hidden files
+alias la='eza -lah --git'
+
+# Tree view
+alias tree='eza --tree'
+
+# Reuse ls completions for eza (avoids defining a separate completion function)
+compdef eza=ls
+
+# Better cat
+#alias cat='bat'
+
+# =========================================================
+# Core utilities
+# =========================================================
+
+alias grep='rg --color=auto'
+alias diff='diff --color=auto'
+alias df='df -h'
+
+# =========================================================
+# Navigation
+# =========================================================
+
+alias -- -='cd -'  # -- prevents - being parsed as a flag; cd - jumps to previous directory
+
+# =========================================================
+# Editor
+# =========================================================
+
+alias vim='nvim'
+
+# =========================================================
+# Git
+# =========================================================
+
+alias glog='PAGER="less -F -X" git log'                              # -F quit if one screen, -X no clear on exit
+alias gadog='PAGER="less -F -X" git log --all --decorate --oneline --graph'
+alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+# =========================================================
+# Video
+# =========================================================
+
+alias stream='mpv av://v4l2:/dev/video4 --fullscreen --demuxer-lavf-o=input_format=mjpeg,framerate=30 --profile=low-latency --untimed'
---
+++ private_dot_config/zsh/plugins.zsh
@@ -0,0 +1,21 @@
+# =========================================================
+# Plugins
+# =========================================================
+
+ZPLUGINDIR="${ZDOTDIR:-$HOME/.config/zsh}/plugins"
+
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+if [ ! -d "$ZINIT_HOME" ]; then
+    mkdir -p "$(dirname $ZINIT_HOME)"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source "${ZINIT_HOME}/zinit.zsh"
+
+zinit ice depth=1; 
+
+zinit light romkatv/powerlevel10k
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light zsh-users/zsh-history-substring-search
