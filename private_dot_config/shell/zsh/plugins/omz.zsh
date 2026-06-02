445bc3925320fedf62bec28fb9711af874f07218
Ranly Huang <et82122@gmail.com>
2026-06-02 09:56:12 +0000

    Refactor(zsh): modulize Zsh manager, plugin and theme

---
+++ private_dot_config/shell/zsh/plugins/omz.zsh
@@ -0,0 +1,47 @@
+# 
+
+installFromGit ohmyzsh/ohmyzsh ${ZMANAGERDIR}
+
+export ZSH="${ZMANAGERDIR}/ohmyzsh"
+export ZSH_CUSTOM="${ZSH}/custom"
+export ZSH_PLUGINS="${ZSH_CUSTOM}/plugins"
+export ZSH_THEMES="${ZSH_CUSTOM}/themes"
+
+installPlugins() {
+    local git_repo=$1
+    local project=$(echo ${git_repo} | cut -d '/' -f 2)
+    installFromGit "${git_repo}" "${ZPLUGINDIR}"
+    plugins+=("${project}")
+    if [[ ! -L "${ZSH_PLUGINS}/${project}" ]]; then
+        echo "Creating symlink for ${project}..."
+        ln -sf "${ZPLUGINDIR}/${project}" "${ZSH_PLUGINS}/${project}"
+    fi
+}
+
+
+plugins=(
+	git
+	z
+)
+
+installPlugins "zsh-users/zsh-autosuggestions"
+installPlugins "zdharma-continuum/fast-syntax-highlighting" 
+installPlugins "zsh-users/zsh-history-substring-search" 
+
+# echo "Plugins: ${plugins[@]}"
+
+installThemes() {
+    local git_repo=$1
+    local project=$(echo ${git_repo} | cut -d '/' -f 2)
+    installFromGit "${git_repo}" "${ZTHEMEDIR}"
+    plugins+=("${project}")
+    if [[ ! -L "${ZSH_THEMES}/${project}" ]]; then
+        echo "Creating symlink for ${project}..."
+        ln -sf "${ZTHEMEDIR}/${project}" "${ZSH_THEMES}/${project}"
+    fi
+}
+
+installThemes "romkatv/powerlevel10k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+source $ZSH/oh-my-zsh.sh
