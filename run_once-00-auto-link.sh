7c055f5e1022ec41532a43555f6ce7d297a00645
Ranly Huang <et82122@gmail.com>
2026-06-08 02:13:26 +0000

    Config(chezmoi): remove .bashrc form auto link

--- run_once-00-auto-link.sh
+++ run_once-00-auto-link.sh
@@ -14,12 +14,14 @@ createLink() {
 
 CONF_HOME="$HOME/.config"
 
+echo
+echo "=== Run auto link... ==="
 # Shell
 SHELL_CONF_HOME="$CONF_HOME/shell"
 # Bash
 BASH_CONF_HOME="$SHELL_CONF_HOME/bash"
 createLink "$BASH_CONF_HOME/profile" "$HOME/.profile"
-createLink "$BASH_CONF_HOME/bashrc" "$HOME/.bashrc"
+#createLink "$BASH_CONF_HOME/bashrc" "$HOME/.bashrc"
 # Zsh
 ZSH_CONF_HOME="$SHELL_CONF_HOME/zsh"
 createLink "$ZSH_CONF_HOME/zshenv" "$HOME/.zshenv"
